c00d0e6eba15a9ac33708bb89553655538fb07a6
pooja <poojaburande222@gmail.com>
2018-09-19 22:08:32 +0530

    Creating  Constructor based Beans in spring.xml (manual beans + Annotations )
    Signed-off-by: pooja <poojaburande222@gmail.com>

--- src/main/java/com/hello/Explore.java
+++ src/main/java/com/hello/Explore.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Explore implements World{
-
+	
+	public void print(){
+		System.out.println("in print of Explore ");
+	}
 	public void enjoy() {
 		System.out.println("Exploring new things here.. i'm Loving it!");
 	}
--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,16 +1,18 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.constr.Constructor;
 import com.hello.Explore;
 import com.hello.HelloWorld;
 import com.hello.World;
 
 
 public class TestApp {
-
+  
 	public static void main(String[] args) {
 			
 			ApplicationContext app = new ClassPathXmlApplicationContext("spring.xml");
+			
 			HelloWorld hello = (HelloWorld) app.getBean("helloWorld");
 			hello.setMessage("Hie");
 			hello.enjoy();
@@ -19,6 +21,11 @@ public class TestApp {
 			
 			World wr=(Explore)app.getBean("explore");
 			wr.enjoy();
+			
+			System.out.println("-----------------------");
+			
+			Constructor Constructor = (Constructor)app.getBean("constructor");
+			
 	}
 
 }
